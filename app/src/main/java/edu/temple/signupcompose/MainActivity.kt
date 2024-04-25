77bac5eeb2be6fc2531ce7082d766abc47230623
Jason Hankins <tul53850@temple.edu>
2024-04-25 11:56:29 -0400

    Remembersaveable allows textfield to save on device rotate

--- app/src/main/java/edu/temple/signupcompose/MainActivity.kt
+++ app/src/main/java/edu/temple/signupcompose/MainActivity.kt
@@ -6,11 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,12 +44,21 @@ class MainActivity : ComponentActivity() {
 fun SignUpForm(name: String, modifier: Modifier = Modifier) {
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
 
+        var userName by rememberSaveable { mutableStateOf("") }
+
         Text(
             text = "Sign Up Form!",
             modifier = modifier,
             fontSize = 28.sp
         )
-        //TextField(value = "", onValueChange = null)
+        TextField(
+            value = userName,
+            label = {Text("Name")},
+            onValueChange = {userName = it}
+        )
+        Button(onClick = { /*TODO*/ }) {
+            
+        }
     }
 }
 
