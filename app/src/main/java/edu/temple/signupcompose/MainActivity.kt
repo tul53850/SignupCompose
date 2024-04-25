2b4c1d62561a3b48cb2bedfe090cf3d17342c357
Jason Hankins <tul53850@temple.edu>
2024-04-25 11:40:08 -0400

    Create signup text

--- app/src/main/java/edu/temple/signupcompose/MainActivity.kt
+++ app/src/main/java/edu/temple/signupcompose/MainActivity.kt
@@ -3,13 +3,18 @@ package edu.temple.signupcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import edu.temple.signupcompose.ui.theme.SignupComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SignUpForm("Android")
                 }
             }
         }
@@ -30,17 +35,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun SignUpForm(name: String, modifier: Modifier = Modifier) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+
+        Text(
+            text = "Sign Up Form!",
+            modifier = modifier,
+            fontSize = 28.sp
+        )
+        //TextField(value = "", onValueChange = null)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SignupComposeTheme {
-        Greeting("Android")
+        SignUpForm("Bro")
     }
 }
